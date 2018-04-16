1135cb1e2c2cac58c65f90d3e2c0e2e7e9eb4732
Pedro Lopes <>
2018-04-15 23:17:36 -0300

    Details page started

---
+++ GitHubSwiftList/GitHubSwiftList/ViewControllers/RepositoryInfoViewControllet.swift
@@ -0,0 +1,17 @@
+//
+//  RepositoryInfoViewControllet.swift
+//  GitHubSwiftList
+//
+//  Created by Pedro Lopes on 15/04/18.
+//  Copyright © 2018 Pedro Lopes. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RepositoryInfoViewController: UIViewController{
+    
+    @IBAction func viewOnBrowser(sender: UIButton){
+        print("Open Browser")
+    }
+}
